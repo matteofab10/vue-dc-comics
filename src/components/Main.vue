5a84b9fed13a9a3e6358aac734ad079390c3df18
matteofab10 <matteofabio10@gmail.com>
2021-11-25 18:16:46 +0100

    esercizio completo

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,14 @@
 <template>
   <main>
     <div class="jumbotron"></div>
-
     <div class="container heroBox">
-
+      <div class="current-series">current series</div>
       <ProductCard 
         v-for="(product, index) in productItems"
         :key="index" 
         :product="product"
         />
-
+      <button>load more</button>
     </div>
   </main>
 </template>
@@ -43,9 +42,35 @@ export default {
       height: 400px;
     }
     .heroBox {
+      position: relative;
       display: flex;
       flex-wrap: wrap;
       justify-content: space-between;
+      padding-top: 60px;
+    }
+    .current-series {
+      position: absolute;
+      top: -35px;
+      left: -10px;
+      padding: 10px 20px;
+      background-color: #0282f9;
+      color: white;
+      text-transform: uppercase;
+      font-size: 30px;
+      font-weight: bold;
+    }
+    button {
+      display: flex;
+      justify-content: center;
+      align-content: center;
+      align-items: center;
+      background-color: #0282f9;
+      color: white;
+      padding: 15px 70px;
+      cursor: pointer;
+      text-transform: uppercase;
+      margin: 30px 0;
+      border: none;
     }
   }
 
