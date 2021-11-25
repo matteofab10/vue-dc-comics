4d399eaf06e607226ccf8b874701382c71040564
matteofab10 <matteofabio10@gmail.com>
2021-11-25 17:28:08 +0100

    stampo ciclo immagini

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="product-card">
+    <img :src="product.thumb" :alt="product.series">
+    <h3>{{product.series}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductCard',
+  props: {
+    product: Object,
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "../assets/style/vars.scss";
+
+  .product-card {
+    width: calc(100% / 6);
+    padding: 10px;
+    img {
+      object-fit: cover;
+      object-position: top;
+      width: 100%;
+      height: 170px;
+    }
+    h3 {
+      color: white;
+    }
+  }
+</style>
